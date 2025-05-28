48e503c067d8caa563d7acbf5c6a471052bae718
Hernan Di Tano <hditano@gmail.com>
2025-05-28 23:58:09 +0200

    first commit

---
+++ go.mod
@@ -0,0 +1,22 @@
+module github.com/hditano/agent
+
+go 1.24.2
+
+require (
+	github.com/jedib0t/go-pretty/v6 v6.6.7
+	github.com/shirou/gopsutil/v4 v4.25.4
+)
+
+require (
+	github.com/ebitengine/purego v0.8.2 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/tklauser/go-sysconf v0.3.12 // indirect
+	github.com/tklauser/numcpus v0.6.1 // indirect
+	github.com/yusufpapurcu/wmi v1.2.4 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+	"time"
+
+	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/shirou/gopsutil/v4/cpu"
+	"github.com/shirou/gopsutil/v4/host"
+	"github.com/shirou/gopsutil/v4/mem"
+	"github.com/shirou/gopsutil/v4/process"
+)
+
+func main() {
+
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			v, _ := mem.VirtualMemory()
+			c, _ := cpu.Percent(time.Second, false)
+			p, _ := process.Pids()
+			h, _ := host.Info()
+
+			mbMemory := v.Total / 1024 / 1024
+			freeMemory := v.Free / 1024 / 1024
+
+			numProcess := len(p)
+			platInformation := h.Platform
+			hostName := h.Hostname
+			cpuModel, _ := cpu.Info()
+
+			ticker := time.NewTicker(1 * time.Second)
+			defer ticker.Stop()
+
+			mainTable(platInformation, hostName, numProcess)
+			hardwareTable(mbMemory, freeMemory, c[0])
+			cpuTable(cpuModel[0].ModelName, cpuModel[0].Model, cpuModel[0].Mhz)
+		}
+	}
+}
+
+func mainTable(platInformation string, hostName string, numProcess int) {
+
+	fmt.Printf("======== Main Stats ========\n")
+	t1 := table.NewWriter()
+	t1.SetOutputMirror(os.Stdout)
+	t1.AppendHeader(table.Row{"O.S", "Platform", "Hostname", "Processes Running"})
+	t1.AppendRows([]table.Row{
+		{runtime.GOOS, platInformation, hostName, numProcess},
+	})
+	t1.Pager()
+	t1.Render()
+}
+
+func hardwareTable(memory uint64, freeMemory uint64, c float64) {
+
+	fmt.Printf("======== Hardware Stats ========\n")
+	t2 := table.NewWriter()
+	t2.SetOutputMirror(os.Stdout)
+	t2.AppendHeader(table.Row{"VM Name", "Total Memory", "Free Memory", "Cpu Usage"})
+	t2.AppendRows([]table.Row{
+		{"test vm", memory, freeMemory, fmt.Sprintf("%.2f", c)},
+	})
+	t2.Render()
+}
+
+func cpuTable(modelName string, model string, speed float64) {
+
+	fmt.Printf("======== CPU Stats ========\n")
+
+	t3 := table.NewWriter()
+	t3.SetOutputMirror(os.Stdout)
+	t3.AppendHeader(table.Row{"Model Name", "Model", "Speed"})
+	t3.AppendRows([]table.Row{
+		{modelName, model, speed},
+	})
+	t3.Pager()
+	t3.Render()
+}
